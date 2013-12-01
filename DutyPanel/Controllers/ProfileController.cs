b0d52db3cc46c84a778b34f24930d07864629164
evgwed <evgwed@gmail.com>
2013-12-01 19:39:20 +0400

    edit information about uset all types work good

--- DutyPanel/Controllers/ProfileController.cs
+++ DutyPanel/Controllers/ProfileController.cs
@@ -36,7 +36,7 @@ namespace DutyPanel.Controllers
                     db.SaveChanges();
                 }
                 ViewData["ErrorText"] = null;
-                return View("Index");
+                return RedirectToAction("Index");
             }
             else
             {
@@ -55,16 +55,6 @@ namespace DutyPanel.Controllers
         }*/
         public ActionResult EditDuty(int id)
         {
-            SelectListItem[] rank_list = new SelectListItem[db.Ranks.Count()];
-            int i = 0;
-            foreach(Rank rank_tmp in db.Ranks)
-            {
-                rank_list[i] = new SelectListItem();
-                rank_list[i].Selected = false;
-                rank_list[i].Text = rank_tmp.Name;
-                rank_list[i].Value = rank_tmp.Id.ToString();
-                i++;
-            }
             ViewData["Rank"] = new SelectList(db.Ranks, "Id", "Name");
             return View(db.Dutys.Find(id));
         }
@@ -76,33 +66,43 @@ namespace DutyPanel.Controllers
             db.Entry(duty_usr).State = System.Data.EntityState.Modified;
             db.SaveChanges();
             Session["User"] = duty_usr;
-            ViewData["InfoText"] = "Данные о пользователе обновлены";
             return RedirectToAction("Index");
         }
-        /*[HttpPost]
-        public ActionResult Edit(DutyPanel.Models.User usr)
+        public ActionResult EditOperativeWorker(int id)
         {
-            db.Entry(usr).State = System.Data.EntityState.Modified;
-            db.SaveChanges();
-            if (usr is AdminUser)
-            {
-                Session["User"] = usr as AdminUser;
-            }
-            if (usr is Duty)
-            {
-                Session["User"] = usr as Duty;
-            }
-            if (usr is OperativeWorker)
-            {
-                Session["User"] = usr as OperativeWorker;
-            }
-            if (usr is Driver)
-            {
-                Session["User"] = usr as Driver;
-            }
-            return RedirectToAction("Index");
-        }*/
+            ViewData["Rank"] = new SelectList(db.Ranks, "Id", "Name");
+            ViewData["Group"] = new SelectList(db.OperationalGroups, "IdGroup", "IdGroup");
+            return View(db.OperativeWorkers.Find(id));
+        }
+        [HttpPost]
+        public ActionResult EditOperativeWorker(DutyPanel.Models.OperativeWorker ow_usr)
+        {
+            ow_usr.Rank = db.Ranks.Find(Convert.ToInt32(Request.Form["Rank"]));
+            ow_usr.Group = db.OperationalGroups.Find(Convert.ToInt32(Request.Form["Group"]));
 
+            db.Entry(ow_usr).State = System.Data.EntityState.Modified;
+            db.SaveChanges();
+            Session["User"] = ow_usr;
+            return RedirectToAction("Index");
+        }
+        public ActionResult EditDriver(int id)
+        {
+            ViewData["Rank"] = new SelectList(db.Ranks, "Id", "Name");
+            ViewData["Group"] = new SelectList(db.OperationalGroups, "IdGroup", "IdGroup");
+            return View(db.Drivers.Find(id));
+        }
+        [HttpPost]
+        public ActionResult EditDriver(DutyPanel.Models.Driver d_usr)
+        {
+            d_usr.Rank = db.Ranks.Find(Convert.ToInt32(Request.Form["Rank"]));
+            d_usr.Group = db.OperationalGroups.Find(Convert.ToInt32(Request.Form["Group"]));
+
+            db.Entry(d_usr).State = System.Data.EntityState.Modified;
+            db.SaveChanges();
+            Session["User"] = d_usr;
+            return RedirectToAction("Index");
+        }
+        
         /// <summary>
         /// profile/creatadmin - создание тестовых пользователей
         /// </summary>
@@ -112,8 +112,24 @@ namespace DutyPanel.Controllers
             if (db.AdminUsers.Count() == 0)
             {
                 db.AdminUsers.Add(new AdminUser() { LastName = "Prohorov", FirstName = "Evgeniy", SecondName = "Eduardovich", PassportNumber = 342234, WhoGivePassport = "UFMS Russia", SubdivisionPasport = 345345, DateGivePassport = DateTime.Parse("12.12.2008"), RegistrationAddress = "Russia", DateOfLastEntry = DateTime.Now, ContactPhone = "+7-344-343-34-34", Email = "evgwed@gmail.com", Password = "110149" });
-                Rank r = new Rank() { Name = "Сержант", Salary = 34.34M };
+                Rank r = new Rank() { Name = "Рядовой полиции", Salary = 5000.00M };
                 db.Ranks.Add(r);
+                db.Ranks.Add(new Rank() { Name = "Младший сержант", Salary = 6000.00M });
+                db.Ranks.Add(new Rank() { Name = "Сержант", Salary = 6500.00M });
+                db.Ranks.Add(new Rank() { Name = "Старший сержант", Salary = 7000.00M });
+                db.Ranks.Add(new Rank() { Name = "Старшина", Salary = 7500.00M });
+                db.Ranks.Add(new Rank() { Name = "Прапорщик", Salary = 8000.00M });
+                db.Ranks.Add(new Rank() { Name = "Старший прапорщик", Salary = 8500.00M });
+                db.Ranks.Add(new Rank() { Name = "Младший лейтенант", Salary = 9500.00M });
+                db.Ranks.Add(new Rank() { Name = "Лейтенант", Salary = 10000.00M });
+                db.Ranks.Add(new Rank() { Name = "Старший лейтенант", Salary = 10500.00M });
+                db.Ranks.Add(new Rank() { Name = "Майор", Salary = 11500.00M });
+                db.Ranks.Add(new Rank() { Name = "Подполковник", Salary = 12000.00M });
+                db.Ranks.Add(new Rank() { Name = "Полковник", Salary = 13000.00M });
+                db.Ranks.Add(new Rank() { Name = "Генерал-майор", Salary = 20000.00M });
+                db.Ranks.Add(new Rank() { Name = "Генерал-лейтенант", Salary = 22000.00M });
+                db.Ranks.Add(new Rank() { Name = "Генерал-полковник", Salary = 25000.00M });
+                db.Ranks.Add(new Rank() { Name = "Генерал", Salary = 27000.00M });
                 db.Dutys.Add(new Duty() {ContactPhone="+3-333-333-33-33", DateGivePassport = DateTime.Parse("23.02.1923"), DateIssueServiceCertificate = DateTime.Parse("23.10.2010"), DateLastEditedRank = DateTime.Parse("11.11.12"), DateRegistr= DateTime.Now, EmployeeNumber=32423, FirstName="Ivan", LastName="Ivanov", NumberServiceCertificate= 23, PassportNumber=34432234, PenaltiesCount=0, RegistrationAddress="Risdss", SecondName="Ilya", SubdivisionPasport=34234, TypeDuty= TypeDuty.Duty, WhoGivePassport="Ya", WorkingPhone="34-34-34", Email="duty@mail.ru", Password="duty", Rank = r});
                 Car cr = new Car() { BrandCar="Mersedes", Color="Hot Red", DateLastRefueling= DateTime.Now, DesiredCategoryDriving="B", FuelType="92,95", IsPlaceFoDetainees= false, IsSpecSignal= true, ModelCar="S600", NumberCar="r345AM", ReleaseDate= DateTime.Now, SeatsCount=5};
                 db.Cars.Add(cr);
