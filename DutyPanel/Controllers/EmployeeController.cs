57b03178d87c66ec4f66f1f2a9e47d6c4e0a9251
evgwed <evgwed@gmail.com>
2013-12-05 17:18:46 +0400

    to merge test and master

---
+++ DutyPanel/Controllers/EmployeeController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DutyPanel.Models;
+
+namespace DutyPanel.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View(db.EmployeeUsers.ToList());
+        }
+
+        //
+        // GET: /Employee/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            EmployeeUser employeeuser = db.EmployeeUsers.Find(id);
+            if (employeeuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeuser);
+        }
+
+        //
+        // GET: /Employee/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employee/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmployeeUser employeeuser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmployeeUsers.Add(employeeuser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employeeuser);
+        }
+
+        //
+        // GET: /Employee/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            EmployeeUser employeeuser = db.EmployeeUsers.Find(id);
+            if (employeeuser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewData["Rank"] = new SelectList(db.Ranks, "Id", "Name");
+            return View(employeeuser);
+        }
+
+        //
+        // POST: /Employee/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EmployeeUser employeeuser)
+        {
+            employeeuser.Rank = db.Ranks.Find(Convert.ToInt32(Request.Form["Rank"]));
+            if (ModelState.IsValid)
+            {
+                db.Entry(employeeuser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employeeuser);
+        }
+
+        //
+        // GET: /Employee/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            EmployeeUser employeeuser = db.EmployeeUsers.Find(id);
+            if (employeeuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeuser);
+        }
+
+        //
+        // POST: /Employee/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmployeeUser employeeuser = db.EmployeeUsers.Find(id);
+            db.EmployeeUsers.Remove(employeeuser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DutyPanel/Controllers/MeritController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DutyPanel.Models;
+
+namespace DutyPanel.Controllers
+{
+    public class MeritController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        //
+        // GET: /Merit/
+
+        public ActionResult Index()
+        {
+            return View(db.Merits.ToList());
+        }
+
+        //
+        // GET: /Merit/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Merit merit = db.Merits.Find(id);
+            if (merit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(merit);
+        }
+
+        //
+        // GET: /Merit/Create
+
+        public ActionResult Create(int id = 0)
+        {
+            if (db.EmployeeUsers.Find(id) != null)
+            {
+                Session["Employee"] = db.EmployeeUsers.Find(id);
+                return View();
+            }
+            else
+            { 
+                return HttpNotFound(); 
+            }
+        }
+
+        //
+        // POST: /Merit/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Merit merit)
+        {
+            merit.Employee = db.EmployeeUsers.Find((Session["Employee"] as EmployeeUser).Id);
+            db.Merits.Add(merit);
+            db.SaveChanges();
+            Session["Employee"] = null;
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Merit/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Merit merit = db.Merits.Find(id);
+            if (merit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(merit);
+        }
+
+        //
+        // POST: /Merit/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Merit merit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(merit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(merit);
+        }
+
+        //
+        // GET: /Merit/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Merit merit = db.Merits.Find(id);
+            if (merit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(merit);
+        }
+
+        //
+        // POST: /Merit/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Merit merit = db.Merits.Find(id);
+            db.Merits.Remove(merit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
